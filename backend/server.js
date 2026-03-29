4b19c60b157802372c8dcf060bc9a3b15624cb28
nandithaprakashot7@gmail.com <nandithaprakashot7@gmail.com>
2026-03-29 15:09:30 +0530

    backend version 1

---
+++ backend/server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection (we'll add real link later)
+mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/test")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Schema
+const textSchema = new mongoose.Schema({
+  text: String,
+  type: String,
+  result: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Text = mongoose.model("Text", textSchema);
+
+// Routes
+
+// POST /format
+app.post("/format", async (req, res) => {
+  const { text, type } = req.body;
+
+  let result = "";
+
+  if (type === "uppercase") result = text.toUpperCase();
+  else if (type === "lowercase") result = text.toLowerCase();
+  else if (type === "reverse") result = text.split("").reverse().join("");
+
+  const newEntry = new Text({ text, type, result });
+  await newEntry.save();
+
+  res.json({ result });
+});
+
+app.get("/", (req, res) => {
+    res.send("API is working 🚀");
+
+  });
+
+  
+
+// GET /history
+app.get("/history", async (req, res) => {
+  const data = await Text.find().sort({ createdAt: -1 });
+  res.json(data);
+});
+
+// Start server
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
